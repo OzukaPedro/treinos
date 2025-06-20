ce3729fea3fe92d86b208787ecb8a4253dc07443
Pedro Campagnoli <pedrocampagnoli28@gmail.com>
2025-06-20 16:36:37 -0300

    sidebar

--- src/app/(public)/dialog/page.tsx
+++ src/app/(public)/dialog/page.tsx
@@ -1,15 +1,76 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import clsx from "clsx";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
+
 export default function DialogPage() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
   return (
-    <div className="flex justify-center items-center h-screen">
-      <dialog open className="modal">
-        <form method="dialog" className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close.</p>
-          <div className="modal-action">
-            <button className="btn">Close</button>
-          </div>
-        </form>
-      </dialog>
+    <div className="flex min-h-screen w-full">
+      <aside
+        className={clsx(
+          "flex flex-col border-r bg-background transition-all duration-300 p-4 h-full",
+          {
+            "w-20": isCollapsed,
+            "w-64": !isCollapsed,
+            "hidden md:flex md:fixed": true,
+          }
+        )}
+      >
+        <Button
+          className="bg-gray-100 hover:bg-gray-50 text-zinc-900 self-end mb-2"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {!isCollapsed ? (
+            <ChevronLeft className="w-12 h-12" />
+          ) : (
+            <ChevronRight className="w-12 h-12" />
+          )}
+        </Button>
+        <div
+          className={clsx("flex justify-center items-center h-screen", {
+            hidden: isCollapsed,
+          })}
+        >
+          <Dialog>
+            <DialogTrigger className="bg-black px-4 py-2 rounded-md">
+              Dialog
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="text-black">
+                  Are you sure absolutely sure?
+                </DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose className="text-black">Cancel</DialogClose>
+                <DialogClose asChild>
+                  <Button onClick={handleToggle}>Continue</Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </aside>
     </div>
   );
 }
